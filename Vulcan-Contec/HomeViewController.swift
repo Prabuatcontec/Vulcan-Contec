d46108be9efdf4019e866d01790345e4d69c3979
Prabu <mprabu@gocontec.com>
2020-12-29 23:30:20 -0800

    Side Meni

--- Vulcan-Contec/HomeViewController.swift
+++ Vulcan-Contec/HomeViewController.swift
@@ -9,42 +9,104 @@
 import UIKit
 import SideMenu
 
-class HomeViewController: UIViewController {
-var _username: String?
+class HomeViewController: UIViewController, MenuListCotrollerDelegate {
+    
+    private var sideMenu: SideMenuNavigationController?
+    private let scanController = ScanViewController()
+    var _username: String?
     override func viewDidLoad() {
         super.viewDidLoad()
+        let menu = MenuListController(with:["Home", "Info", "SN Detail"])
+        sideMenu = SideMenuNavigationController(rootViewController: menu )
+        menu.delegate = self
         title = "Home"
         username.text = _username
-        SideMenuNavigationController(rootViewController: MenuListController()).leftSide = true
+        sideMenu?.leftSide = true
         
-        SideMenuManager.default.leftMenuNavigationController = SideMenuNavigationController(rootViewController: MenuListController())
+        SideMenuManager.default.leftMenuNavigationController = sideMenu
         SideMenuManager.default.addPanGestureToPresent(toView: self.view)
+        addChildController()
         // Do any additional setup after loading the view.
     }
     @IBAction func didTapMenu()
     {
-        present(SideMenuNavigationController(rootViewController: MenuListController()), animated: true)
+        present(sideMenu!, animated: true)
+    }
+    
+    private func addChildController() {
+        addChild(scanController)
+        view.addSubview(scanController.view)
+        scanController.view.frame = view.bounds
+        scanController.didMove(toParent: self)
+        scanController.view.isHidden = true
+    }
+    
+    func didSelectMenuItem(named: String) {
+        sideMenu?.dismiss(animated: true, completion: { [weak self] in
+            
+            self?.title = named
+            
+            if named == "Home"{
+                self?.view.backgroundColor = .blue
+                self?.scanController.view.isHidden = true
+            } else if named == "Info" {
+                self?.view.backgroundColor = .red
+                self?.scanController.view.isHidden = true
+            } else if named == "SN Detail" {
+                self?.view.backgroundColor = .green
+                self?.scanController.view.isHidden = false
+            }
+        })
     }
 
     @IBOutlet weak var username: UILabel!
     
-    class MenuListController: UITableViewController {
-        var lists = ["First", "Second", "Third"]
-        
-        override func viewDidLoad() {
-            super.viewDidLoad()
-            tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
-        }
-        
-        override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-            return lists.count
-        }
-        
-        override func tableView(_ tableView: UITableView, cellForRowAt indexpath: IndexPath) -> UITableViewCell {
-            let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexpath)
-            cell.textLabel?.text = lists[indexpath.row]
-            return cell
-        }
-    }
+   
 
+}
+
+protocol MenuListCotrollerDelegate {
+    func didSelectMenuItem(named: String)
+}
+
+class MenuListController: UITableViewController {
+    private let  lists : [String]
+    
+    public var delegate: MenuListCotrollerDelegate?
+    init(with lists:[String]) {
+        self.lists = lists
+        super.init(nibName: nil, bundle: nil)
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        tableView.backgroundColor = UIColor(red: 48/255, green: 155/255, blue: 255/255, alpha: 1)
+        view.backgroundColor = UIColor(red: 48/255, green: 155/255, blue: 255/255, alpha: 1)
+        
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return lists.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexpath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexpath)
+        cell.textLabel?.text = lists[indexpath.row]
+        cell.textLabel?.textColor = .white
+        cell.backgroundColor = UIColor(red: 48/255, green: 155/255, blue: 255/255, alpha: 1)
+        cell.contentView.backgroundColor = UIColor(red: 48/255, green: 155/255, blue: 255/255, alpha: 1)
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+        let selectedItem = lists[indexPath.row]
+        delegate?.didSelectMenuItem(named: selectedItem)
+        
+    }
 }
--- Vulcan-Contec/ViewController.swift
+++ Vulcan-Contec/ViewController.swift
@@ -13,9 +13,25 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        let newButton = UIButton()
+        newButton.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(newButton)
+        newButton.layer.cornerRadius = 12
+        newButton.backgroundColor = UIColor(red: 48/255, green: 155/255, blue: 255/255, alpha: 1)
+        newButton.setTitle("Login", for: .normal)
+        let constra = [
+        newButton.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -320),
+        newButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+        newButton.widthAnchor.constraint(equalToConstant: 200),
+        newButton.heightAnchor.constraint(equalToConstant: 60)
+        ]
+        NSLayoutConstraint.activate(constra)
+        
+        newButton.addTarget(self, action: #selector(self.Login(_:)), for: .touchUpInside)
         // Do any additional setup after loading the view.
     }
     
+    @IBOutlet weak var loginButton: UIButton!
     @IBAction func username(_ sender: Any) {
     }
     
@@ -23,8 +39,17 @@ class ViewController: UIViewController {
     }
     @IBOutlet weak var usename: UITextField!
     
+    fileprivate func animateView(_ viewToAnimate:UIView){
+        UIView.animate(withDuration: 0.15, delay: 0, usingSpringWithDamping: 0.2, initialSpringVelocity: 0.5, options: .curveEaseIn, animations: { viewToAnimate.transform =
+            CGAffineTransform(scaleX: 0.92, y: 0.92)
+        } ) { (_) in
+            print("comp")
+        }
+    }
+    
     @IBOutlet weak var password: UITextField!
-    @IBAction func Login(_ sender: Any) {
+    @IBAction func Login(_ sender: UIButton) {
+        self.animateView(sender)
         guard let _username = usename.text else { return };
         guard let _password = password.text else { return };
         let modLogin = LoginModel(username: _username, password: _password)
